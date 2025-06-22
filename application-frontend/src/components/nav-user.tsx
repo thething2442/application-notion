5d53a8c5afbd9ef56ab105660a5f35390eaeaef2
DevServer24 <developmentserver160@gmail.com>
2025-06-22 19:51:41 +0800

    added

--- application-frontend/src/components/nav-user.tsx
+++ application-frontend/src/components/nav-user.tsx
@@ -28,7 +28,7 @@ export function NavUser() {
               className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
             >
               <Avatar className="h-8 w-8 rounded-lg">
-                <AvatarImage src={user?.imageUrl} alt={user?.firstName} />
+                <AvatarImage src={user?.imageUrl} alt={user?.firstName ?? undefined} />
                 <AvatarFallback className="rounded-lg">{user?.firstName?.charAt(0)}</AvatarFallback>
               </Avatar>
               <div className="grid flex-1 text-left text-sm leading-tight">
@@ -47,7 +47,7 @@ export function NavUser() {
             <DropdownMenuLabel className="p-0 font-normal">
               <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
                 <Avatar className="h-8 w-8 rounded-lg">
-                  <AvatarImage src={user?.imageUrl} alt={user?.firstName} />
+                  <AvatarImage src={user?.imageUrl} alt={user?.firstName ?? undefined} />
                   <AvatarFallback className="rounded-lg">{user?.firstName?.charAt(0)}</AvatarFallback>
                 </Avatar>
                 <div className="grid flex-1 text-left text-sm leading-tight">
