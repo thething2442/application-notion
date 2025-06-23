e1b5ea5e648be8348aedc2cffa574ece3f2fcaf5
DevServer24 <developmentserver160@gmail.com>
2025-06-24 04:40:18 +0800

    added cors unban url

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,9 +8,13 @@ async function bootstrap() {
   // Enable CORS
   app.enableCors({
     origin: [
+      'application-notion-rtm4-git-main-jade-leonard-alvezs-projects.vercel.app',
+      'application-notion-rtm4.vercel.app',
+      'application-notion-rtm4-f95sv00bn-jade-leonard-alvezs-projects.vercel.app',
       'http://localhost:3000', // Frontend development server
       'http://localhost:3001', // Alternative frontend port
       'https://your-production-domain.com', // Add your production domain
+      
     ],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: [
